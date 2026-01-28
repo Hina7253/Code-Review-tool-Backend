8b6e16f1655d88d89c6ac4087aa94f82280db71e
Hina <hinakumari.725394@gmail.com>
2026-01-28 21:06:32 +0530

    add fileanalysisresult

--- src/main/java/com/codereview/model/FileAnalysisResult.java
+++ src/main/java/com/codereview/model/FileAnalysisResult.java
@@ -1,5 +1,46 @@
 package com.codereview.model;
 
 public class FileAnalysisResult {
+    private String fileName;
+    private AnalysisResponse analysis;
+    private CodeMetrics metrics;
+    private int totalIssue;
 
+    public FileAnalysisResult() {}
+
+    public FileAnalysisResult(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public AnalysisResponse getAnalysis() {
+        return analysis;
+    }
+
+    public void setAnalysis(AnalysisResponse analysis) {
+        this.analysis = analysis;
+    }
+
+    public CodeMetrics getMetrics() {
+        return metrics;
+    }
+
+    public void setMetrics(CodeMetrics metrics) {
+        this.metrics = metrics;
+    }
+
+    public int getTotalIssue() {
+        return totalIssue;
+    }
+
+    public void setTotalIssue(int totalIssue) {
+        this.totalIssue = totalIssue;
+    }
 }
