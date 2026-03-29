f1ba7d4743c31d5629334622381da9dba48bd0f6
Hina <hinakumari.725394@gmail.com>
2026-03-29 15:54:42 +0530

    update

--- src/main/java/com/codereview/security/UserDetailsImpl.java
+++ src/main/java/com/codereview/security/UserDetailsImpl.java
@@ -1,4 +1,25 @@
 package com.codereview.security;
 
-public class UserDetailsImpl {
-}
+import com.codereview.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(Long id, String userna
