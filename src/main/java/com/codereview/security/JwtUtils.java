5811782a8c6bad5feb06a4a336e1ee5841d266fd
Hina <hinakumari.725394@gmail.com>
2026-03-29 15:41:25 +0530

    update

--- src/main/java/com/codereview/security/JwtUtils.java
+++ src/main/java/com/codereview/security/JwtUtils.java
@@ -1,4 +1,12 @@
 package com.codereview.security;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpirationMs;
 }
