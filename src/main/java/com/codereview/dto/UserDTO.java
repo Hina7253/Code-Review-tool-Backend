4f638bfc3b8fae30863f3cd963922c5400c3d85a
Hina <hinakumari.725394@gmail.com>
2026-04-04 07:52:02 +0530

    update usredto

--- src/main/java/com/codereview/dto/UserDTO.java
+++ src/main/java/com/codereview/dto/UserDTO.java
@@ -1,4 +1,13 @@
+// UserDTO.java
 package com.codereview.dto;
 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
 public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
 }
